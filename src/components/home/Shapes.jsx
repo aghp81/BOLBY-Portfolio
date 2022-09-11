5dfc077559ad80421e469672121f1f0bfe258326
abol ghana <aghp81@gmail.com>
2022-09-11 19:59:39 +0430

    shapes

---
+++ src/components/home/Shapes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Shapes = () => {
+    return (
+        <div className="shapes">
+            <svg class="shape s1" width="20" height="100">
+                <circle cx="50" cy="30" r="40" stroke="red" stroke-width="1" fill="yellow" />
+            </svg>
+
+            
+
+            
+        </div>
+    )
+}
+
+export default Shapes
