a6f84e75c0371a16b43fee980b4e3a1f726969b8
abol ghana <aghp81@gmail.com>
2022-09-13 20:54:18 +0430

    About Box

--- src/components/about/AboutBox.jsx
+++ src/components/about/AboutBox.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 const AboutBox = () => {
     return (
-        <div className="about__boxes grid"></div>
+        <div className="about__boxes grid">
+            <div className="about__box">
+                <i className="about__icon"></i>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
     )
 }
 
