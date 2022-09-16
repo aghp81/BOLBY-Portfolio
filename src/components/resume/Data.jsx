e89aef4a0282cd05eb280ee33a67754dcbb2352f
abol ghana <aghp81@gmail.com>
2022-09-16 11:43:09 +0430

    Resume Data

---
+++ src/components/resume/Data.jsx
@@ -0,0 +1,53 @@
+const Data = [
+    {
+        id: 1,
+        category: "education",
+        icon: "icon-graduation",
+        year: "2019 - present",
+        title: "Academic Degree",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam excepturi in debitis nisi cupiditate maiores culpa dicta illum suscipit officia.",
+    },
+    {
+        id: 2,
+        category: "education",
+        icon: "icon-graduation",
+        year: "2019 - present",
+        title: "Academic Degree",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam excepturi in debitis nisi cupiditate maiores culpa dicta illum suscipit officia.",
+    },
+    {
+        id: 3,
+        category: "education",
+        icon: "icon-graduation",
+        year: "2013 - 2017",
+        title: "Bachelor's Degree",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam excepturi in debitis nisi cupiditate maiores culpa dicta illum suscipit officia.",
+    },
+    {
+        id: 4,
+        category: "experience",
+        icon: "icon-briefaces",
+        year: "2019 - present",
+        title: "Web Designer",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam excepturi in debitis nisi cupiditate maiores culpa dicta illum suscipit officia.",
+    },
+    {
+        id: 5,
+        category: "experience",
+        icon: "icon-briefaces",
+        year: "2013 - 2017",
+        title: "Front-End Developer",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam excepturi in debitis nisi cupiditate maiores culpa dicta illum suscipit officia.",
+    },
+    {
+        id: 4,
+        category: "experience",
+        icon: "icon-briefaces",
+        year: "2019 - present",
+        title: "Back-End Developer",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam excepturi in debitis nisi cupiditate maiores culpa dicta illum suscipit officia.",
+    },
+];
+
+
+export default Data
--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import "./resume.css";
+import Data from "./Data";
 
 const Resume = () => {
     return (
-        <div>Resume</div>
+        <section className="resume container section" id="resume">
+            <h2 className="section__title">Experience</h2>
+
+            <div className="resume__container grid">
+                <div className="timeline grid"></div>
+            </div>
+        </section>
     )
 }
 
