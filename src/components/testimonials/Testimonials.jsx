6e4576f237321f5f40cb106f457b2836bab7baf4
abol ghana <aghp81@gmail.com>
2022-09-18 21:48:24 +0430

    Testimonials.jsx

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,46 @@
 import React from "react";
 import "./testimonials.css";
+import Image1 from "../../assets/avatar-1.svg";
+import Image3 from "../../assets/avatar-2.svg";
+
+
+const data = [
+    {
+        id: 1,
+        image: Image1,
+        title: "John Doe",
+        subtitle: "Product designer at Dribble",
+        comment: "I enjoy working with the theme and learn so much. You guys make the process fun and interesting. Good luck!",
+    },
+    {
+        id: 2,
+        image: Image3,
+        title: "John Doe",
+        subtitle: "Product designer at Dribble",
+        comment: "I enjoy working with the theme and learn so much. You guys make the process fun and interesting. Good luck!",
+    },
+];
 
 const Testimonials = () => {
     return (
-        <dive>Testimonials</dive>
+        <section className="testimonials container section">
+            <h2 className="section__title">Clients & Reviews</h2>
+
+            <div className="testimonials__container grid">
+                {data.map(({id, image, title, subtitle, comment}) => {
+                    return (
+                        <div className="testimonial__item" key={id}>
+                            <div className="thumb">
+                                <img width="100" height="100" src={image} alt="" />
+                            </div>
+                            <h3 className="testimonials__title">{title}</h3>
+                            <span className="subtitle">{subtitle}</span>
+                            <div className="comment">{comment}</div>
+                        </div>
+                    )
+                } )}
+            </div>
+        </section>
     )
 }
 
